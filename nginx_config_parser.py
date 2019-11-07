18dc8497d008471e0a56314bc2cb567b8130ac85
Stacy Danilova <stacydanilova@Stacys-MacBook-Pro.local>
2019-11-07 23:49:48 +0300

    added regex

--- nginx_config_parser.py
+++ nginx_config_parser.py
@@ -3,8 +3,38 @@
 import re
 
 file_name = "nginx.conf"
+regex_address = r"\s*(\w?.*)\s*:([0-9]*)[;]"
+regex_listen = r".*listen\s*([0-9]*)[;]"
+regex_who_is_listening = r".*[{].[#]\s(.*)"
+def check_regex(line):
+    address_matches = re.match(regex_address,line)
+    if address_matches:
+        who = address_matches.group(1)
+        port = address_matches.group(2)
+        message = "Port {} is listening by {}".format(port, who)
+        return dict(success=True,listener_unknown=False,message=message)
+    listen_matches = re.match(regex_listen,line)
+    if listen_matches:
+        port = listen_matches.group(1)
+        message = "Port {} is listening".format(port)
+        return dict(success=True,listener_unknown=True,message=message)
+    listener_matches = re.match(regex_who_is_listening,line)
+    if listener_matches:
+        listener = listener_matches.group(1)
+        message = "listener is {}".format(listener)
+        return dict(success=False,listener_unknown=False,message=message)
+    return dict(success=False,listener_unknown=True,message="")
 
-with open(file_name) as fp:
-   line = fp.readline()
-   while line:
-        print line
+lines = [line.rstrip('\n') for line in open(file_name)]
+
+previous_message = ""
+for line in lines:
+    response = check_regex(line)
+    if not response["success"]:
+        previous_message = response["message"]
+        continue
+    if response["listener_unknown"]:
+        print response["message"] + " " + previous_message
+    else:
+        print response["message"]
+    previous_message = response["message"]
