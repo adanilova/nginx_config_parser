26e06e65e815eba8ae5b7d92029e5e67f56b8ce6
Stacy Danilova <stacydanilova@Stacys-MacBook-Pro.local>
2019-11-08 20:49:31 +0300

    get all pairs

--- nginx_config_parser.py
+++ nginx_config_parser.py
@@ -11,17 +11,34 @@ regex_listen = r"^\s*listen\s*((\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}|localhost|
 
 def parse_listen(line):
     matches = re.match(regex_listen,line)
+    ip_address = None
+    port = None
+    success = False
     if matches:
         ip_address = matches.group(2)
         port = matches.group(3)
-        print "IP_ADDRESS: {}    PORT: {}".format(ip_address, port)
+        success = True
+    return dict(ip_address=ip_address, port=port, success=success)
+
+
+def ping(ip_address, port):
+    print "ping {}:{}".format(ip_address, port)
 
 
 def main():
     
+    all_pairs = []
+
     lines = [line.rstrip('\n') for line in open("test.txt")]
-    for line in lines:
-        parse_listen(line)
+    for line in lines:        
+        result = parse_listen(line)
+        if not result["success"]:
+            continue
+
+        ip_address = result["ip_address"] or "0.0.0.0"
+        port = result["port"] or "80"
+
+        ping(ip_address, port)
 
 
 if __name__ == "__main__":
