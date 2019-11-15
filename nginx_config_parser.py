0fa1122c46c52ec024cba6c72d086d2214e93bc7
Stacy Danilova <stacydanilova@Stacys-MacBook-Pro.local>
2019-11-15 21:32:16 +0300

    big changes to script code (before debug)

--- nginx_config_parser.py
+++ nginx_config_parser.py
@@ -5,104 +5,171 @@ import re
 import requests
 import psutil
 
-main_config = "/etc/nginx/nginx.conf"
-config_dirs = ["/etc/nginx/conf.d/", "/etc/nginx/sites-enabled/"]
+# nginx main config files and hosts directories
+MAIN_CONFIG = "/etc/nginx/nginx.conf"
+CONFIG_DIRS = ["/etc/nginx/conf.d/", "/etc/nginx/sites-enabled/"]
 
-regex_listen = r"^\s*listen\s*(((\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}|localhost|[*])[:]?)?(\d{1,65535})?);"
-regex_nginx = r"^.*nginx.*$"
+# nginx process program name
+NGINX_PROGRAM_NAME = "nginx"
+
+# regex for parse "listen" directive in configs
+REGEX_LISTEN = r"^\s*listen\s*(((\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}|localhost|[*])[:]?)?(\d{1,65535})?);"
 
 
-def find_nginx_listen():
+# class for host address
+class HostAddress:
+    def __init__(self, ip_address, port):
+        self.ip_address = ip_address
+        self.port = port
+
+
+# 1. main script function
+def main():
+    # all nginx config files path
+    files_paths = []
+    # check main config file exists and it is a file
+    if path.exists(MAIN_CONFIG) and path.isfile(MAIN_CONFIG):
+        files_paths.append(MAIN_CONFIG)
+
+    # for each config directory
+    for dir_path in CONFIG_DIRS:
+        # check hosts directories exist and they are directories
+        if not path.exists(dir_path) or not path.isdir(dir_path):
+            continue
+        # get all directory child files paths
+        sub_files_paths = [path.join(dir_path, f) for f in listdir(
+            dir_path) if path.isfile(path.join(dir_path, f))]
+        # append these files to config files path array
+        files_paths = files_paths + sub_files_paths
+
+    # pool of all founded hosts
+    hosts_pool = []
+    # for each file in nginx config files array
+    for file_path in files_paths:
+        # all hosts that were parsed in config file
+        file_hosts = parse_file(file_path)
+        # if there are no hosts - skip to the next file
+        if not file_hosts:
+            continue
+        # append parsed hosts to hosts pool
+        hosts_pool.append(file_hosts)
+
+    # distinct array
+    hosts_pool = list(set(hosts_pool))
+    # return all founded hosts with nginx owner flag
+    return check_for_nginx(hosts_pool)
+
+
+# 2. parse config file
+def parse_file(file_path):
+    # all founded hosts in file
+    file_hosts = []
+    # get all config file lines
+    lines = [line.rstrip('\n') for line in open(file_path)]
+    # for each line
+    for line in lines:
+        # parse line to check there is "listen" directive
+        host = parse_listen(line)
+        # if there is no "listen" directive - skip to the next line
+        if not host:
+            continue
+        # append founded host to config file founded hosts array
+        file_hosts.append(host)
+
+    # return all founded hosts
+    return file_hosts
+
+
+# 3. parse line from config file for "listen" directive. Returns None if not parsed
+def parse_listen(line):
+    # apply regex to the line
+    matches = re.match(REGEX_LISTEN, line)
+    # return None if there is no matches
+    if not matches:
+        return None
+    # set ip_address from group, if there is None set to default
+    ip_address = matches.group(3) or "0.0.0.0"
+    # set port from group, if there is None set to default
+    port = matches.group(4) or "80"
+    # handle another default ip_address value
+    if ip_address == "*":
+        ip_address = "0.0.0.0"
+
+    # return parsed host
+    return HostAddress(ip_address, port)
+
+
+# 4. check hosts in hosts pool is nginx process listening them
+def check_for_nginx(hosts_pool):
+    # result output
     result = []
-    connections = psutil.net_connections()
-    for connection in connections:
-        pid = connection.pid
-        if not pid:
-            continue
-        process = psutil.Process(pid)
-        if not process:
-            continue
-        programm = process.name()
-        if programm != "nginx":
-            continue
-        ip_address = connection.laddr.ip
-        port = "{}".format(connection.laddr.port)
-        result.append(dict(ip_address=ip_address, port=port))
+    # all hosts that nginx process is listening
+    nginx_hosts = get_nginx_hosts()
+
+    # for each host in parsed hosts pool
+    for host in hosts_pool:
+        # flag host was found in nginx hosts
+        flag = False
+
+        # check nginx listen 0.0.0.0 with same port
+        if HostAddress("0.0.0.0", host.port) in nginx_hosts:
+            flag = True
+        else:
+            # check nginx listen host ip_address and port
+            flag = host in nginx_hosts
+
+        print_host(host, flag)
+        result.append(dict(host=host, flag=flag))
+
     return result
 
 
-nginx_items = find_nginx_listen()
-
-
-def parse_listen(line):
-    matches = re.match(regex_listen, line)
-    ip_address = None
-    port = None
-    success = False
-    if matches:
-        ip_address = matches.group(3)
-        port = matches.group(4)
-        success = True
-    return dict(ip_address=ip_address, port=port, success=success)
-
-
-def parse_nginx(line):
-    match = re.match(regex_nginx, line)
-    flag = 1 if match else 0
-    return flag
-
-
-def check_nginx_owner(ip_address, port):
-    looking_item = dict(ip_address=ip_address, port=port)
-    return 1 if looking_item in nginx_items else 0
-
-
-def parse_file(file_path):
-    file_results = []
-    lines = [line.rstrip('\n') for line in open(file_path)]
-    for line in lines:
-        result = parse_listen(line)
-        if not result["success"]:
+# 5. get all hosts that nginx process is listening
+def get_nginx_hosts():
+    nginx_hosts = []
+    # get all net connections
+    connections = psutil.net_connections()
+    # for each connection
+    for connection in connections:
+        # try to get nginx host from net connection
+        nginx_host = get_nginx_host(connection)
+        # if nginx_host is None - skip to the next connection
+        if not nginx_host:
             continue
+        # append nginx_host to nginx hosts array
+        nginx_hosts.append(nginx_host)
 
-        ip_address = result["ip_address"] or "0.0.0.0"
-        port = result["port"] or "80"
-
-        if ip_address == "*":
-            ip_address = "0.0.0.0"
-
-        is_nginx_owner = check_nginx_owner(ip_address, port)
-
-        print "{}:{} {}".format(ip_address,
-                                port, is_nginx_owner)
-
-        info = dict(ip_address=ip_address, port=port, flag=is_nginx_owner)
-        file_results.append(info)
-    return file_results
+    return nginx_hosts
 
 
-def main():
-    output = []
-    files_paths = []
+# 6. get host from nginx net connection. Returns None if not nginx process
+def get_nginx_host(connection):
+    # get connection PID, check for None
+    pid = connection.pid
+    if not pid:
+        None
+    # get full process info by PID
+    process = psutil.Process(pid)
+    if not process:
+        None
+    # get process program name
+    programm = process.name()
+    if programm != NGINX_PROGRAM_NAME:
+        None
+    # get local ip_address
+    ip_address = connection.laddr.ip
+    # get local port
+    port = "{}".format(connection.laddr.port)
 
-    if path.exists(main_config) and path.isfile(main_config):
-        files_paths.append(main_config)
-
-    for dir_path in config_dirs:
-        if not path.exists(dir_path) or not path.isdir(dir_path):
-            continue
-        sub_files_paths = [path.join(dir_path, f) for f in listdir(
-            dir_path) if path.isfile(path.join(dir_path, f))]
-        files_paths = files_paths + sub_files_paths
-
-    for file_path in files_paths:
-        file_results = parse_file(file_path)
-        if not file_results:
-            continue
-        output.append(file_results)
-
-    return output
+    # return nginx host
+    return HostAddress(ip_address, port)
 
 
+# 7. print host result output
+def print_host(host, flag):
+    print "{}:{} {}".format(host.ip_address, host.port, flag)
+
+
+# main script scope
 if __name__ == "__main__":
     main()
